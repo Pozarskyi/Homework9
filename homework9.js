51fc4aeb7dd9097a8da33170dd8fa50b38450951
Evgenii <pozar1288@gmail.com>
2022-12-26 13:00:21 +0200

    Homework9

---
+++ homework9.js
@@ -0,0 +1,32 @@
+$(function(){
+    // task1
+    // let href = $('a').attr('href')
+    // console.log(href)
+    // if(href = 'https://'){
+    //     $("a").attr("target", "_blank")
+    // }
+
+    // task2
+    // $('.head').css('background', 'green')
+    // $('.inner').css("font-size", '35px')
+
+    // task3
+
+    // $('div').each(function() {
+    //     var $elem = $(this);
+    //     $elem.prev().before($elem);
+    // });
+    
+
+    // task4
+
+//    let $elem = $('input');
+//    $elem.on('click', function() {
+//     if($('input:checked').length === 3){
+//         $elem.attr('disabled', 'true');
+//     }
+//    })
+    
+
+    
+})
